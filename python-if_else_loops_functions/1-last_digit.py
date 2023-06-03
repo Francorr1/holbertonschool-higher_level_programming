1dba87dd8d0f11b298b79e80797f6471e1789e18
Franco <francorr240@gmail.com>
2023-06-03 14:24:51 -0300

    Fixed styling

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,4 @@ elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
     print(f"Last digit of {number} is {last_digit} and is less tha\
-          n 6 and not 0")
+n 6 and not 0")
