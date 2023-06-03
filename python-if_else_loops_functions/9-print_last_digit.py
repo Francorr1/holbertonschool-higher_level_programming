806689fc07e99af59f5641939c1f94a07b64bd76
Franco <francorr240@gmail.com>
2023-06-03 15:12:08 -0300

    Task 9 complete

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return (number % 10)
+    last = number
+    if number < 0:
+        last *= -1
+    last %= 10
+    print("{}".format(last), end='')
+    return last
