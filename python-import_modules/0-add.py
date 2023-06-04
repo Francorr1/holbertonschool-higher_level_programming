df504ed8bfc26e61cd53bf292bcfed864067417f
Franco <francorr240@gmail.com>
2023-06-04 16:24:42 -0300

    Task 0 complete

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0 as add
-func = add.add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, func(a, b)))
+def add():
+    import add_0 as add
+    func = add.add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, func(a, b)))
