9ce0329fbf6e02900a8f09c6e9e7b104e6a5b591
Franco <francorr240@gmail.com>
2023-06-10 19:11:57 -0300

    Task 10 complete

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best = 0
     if not a_dictionary:
         return None
-    for key, value in a_dictionary.items():
-        if value > best:
-            best = value
-    return best
+    return max(a_dictionary, key=a_dictionary.get)
