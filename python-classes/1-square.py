0f41cec4e899f5736fc68e3b33d28d0af5320dfe
Franco <francorr240@gmail.com>
2023-06-13 12:11:02 -0700

    Task 1 complete

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a square class
+"""
+
+
+class square:
+    """
+    define private attribute size
+    """
+    def __init__(self, size):
+        """
+        define the size
+        """
+        self.__size = size
