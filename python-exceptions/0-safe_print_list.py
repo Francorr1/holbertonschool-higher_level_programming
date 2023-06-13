bdfde34c9328c513d85cdbd2fbce6aa9d4c1f1e5
Franco <francorr240@gmail.com>
2023-06-13 02:19:09 -0300

    Task 0 complete

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
+    i = 0
+    while i < x:
         try:
             print("{}".format(my_list[i]), end="")
         except IndexError:
             break
+        i += 1
     print()
     return i
