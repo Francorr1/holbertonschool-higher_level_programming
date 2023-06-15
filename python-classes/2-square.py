bb92afff2ecebe541eace4258fdf4d707a6b9a5f
Franco <francorr240@gmail.com>
2023-06-15 07:16:08 -0700

    Task 2 complete

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A square class """
+
+
+class Square:
+    """ Define private atribute size """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if int(size) < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    pass
