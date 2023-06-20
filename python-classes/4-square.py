29f6dd9231cc3d090a296f7a9231a3dc12ae69c1
Franco <francorr240@gmail.com>
2023-06-20 13:34:16 -0300

    Task 4 complete

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A square class """
+
+
+class Square:
+    """ Define private atribute size """
+    def __init__(self, size=0):
+        self.__size = size
+
+    """ Recovers the size value """
+    @property
+    def size(self):
+        return self.__size
+
+    """ Sets the size value """
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    """ Calculates the are of a aquare instance """
+    def area(self):
+        return (self.__size ** 2)
