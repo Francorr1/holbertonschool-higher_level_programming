de52fbda5a55b9f0bb8d0fd8b612aadb48844d20
Franco <francorr240@gmail.com>
2023-06-26 14:17:41 -0700

    Task 5 complete

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ save_to_json_file function """
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(json.dumps(my_obj))
