9f306ff00f392dd84a247137ed2b5bd4013105cb
Franco <francorr240@gmail.com>
2023-06-26 23:15:31 -0700

    Task 9 complete

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,14 +3,11 @@
 
 
 class Student:
-    """ Student class """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
-def to_json(self):
-    """ to_json function """
-    if isinstance(self, Student):
-        return self.__dict__
+    def to_json(self):
+        if isinstance(self, Student):
+            return self.__dict__
